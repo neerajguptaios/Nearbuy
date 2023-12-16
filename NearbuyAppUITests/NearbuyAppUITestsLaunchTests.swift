7af3501fd6e86e4bd0338b00b6e71c962a5d0bc5
Neeraj Gupta <neeraj.gupta1@timesinternet.in>
2023-12-16 11:50:30 +0530

    Initial Commit

---
+++ NearbuyAppUITests/NearbuyAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  NearbuyAppUITestsLaunchTests.swift
+//  NearbuyAppUITests
+//
+//  Created by Neeraj Gupta on 16/12/23.
+//
+
+import XCTest
+
+final class NearbuyAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
